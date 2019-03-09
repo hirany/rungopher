6ac6a87db7f74c617972f4d5c129490d70edcc1f
hirany <s17t260@stu.kagawa-u.ac.jp>
2019-03-09 17:23:11 +0900

    何か修正した

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ func runGopher() error {
 	w, h := termbox.Size()
 
 	var aa []rune
+	// ʕ◔ϖ◔ʔ
 	aa = append(aa, 'ʕ')
 	aa = append(aa, '◔')
 	aa = append(aa, 'ϖ')
@@ -48,7 +49,7 @@ func runGopher() error {
 	for i := 0; i < w; i++ {
 		termbox.Clear(backColor, backColor)
 		for k := range aa {
-			termbox.SetCell(w-i+k, h/2, aa[k], backColor, runeColor)
+			termbox.SetCell(w-i+k, h/2, rune(aa[k]), backColor, runeColor)
 		}
 		termbox.Flush()
 		time.Sleep(30 * time.Millisecond)
